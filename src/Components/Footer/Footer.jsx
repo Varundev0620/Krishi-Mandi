f5e68b5d1a9bcd940c353333128feb818be1772f
Varun <Varundev@gmail.com>
2024-03-23 13:54:02 +0530

    added Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,21 +1,76 @@
 import React from 'react'
 import './Footer.css';
-const Footer = () => {
+// import 'https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap';
+
+function Footer() {
   return (
-   <footer>
-        <div className="FooterSection">
-            <div className="rightreserved">
-                <p>2024 krishi Mandi. All Right Reserved </p>
-            </div>
-
-            <div className="termandservice">
-                <p>Term and Services</p>
-                <p>Privacy policy</p>
-                <p>Cookies</p>
-            </div>
-
+    <div>
+        <body>
+  <footer>
+    <div class="content">
+      <div class="top">
+        <div class="logo-details">
+          <i class="fab fa-slack"></i>
+          <span class="logo_name">Krishi Mandi</span>
         </div>
-   </footer>
+        <div class="media-icons">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+      <div class="link-boxes">
+        <ul class="box">
+          <li class="link_name">Company</li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Contact us</a></li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Blog & News</a></li>
+        </ul>
+        <ul class="box">
+          <li class="link_name">Services</li>
+          <li><a href="#">Plans & Pricing</a></li>
+          <li><a href="#">Personal AI</a></li>
+          <li><a href="#">AI Data Processing</a></li>
+          <li><a href="#">AI Business writer</a></li>
+        </ul>
+        <ul class="box">
+          <li class="link_name">Account</li>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">My account</a></li>
+          <li><a href="#">Prefrences</a></li>
+          <li><a href="#">Purchase</a></li>
+        </ul>
+        <ul class="box">
+          <li class="link_name">Resources</li>
+          <li><a href="#">Documentation</a></li>
+          <li><a href="#">Free Demo</a></li>
+          <li><a href="#">Crop Variety</a></li>
+          <li><a href="#">Photoshop</a></li>
+        </ul>
+        <ul class="box input-box">
+          <li class="link_name">Subscribe</li>
+          <li><input type="text" placeholder="Enter your email"/></li>
+          <li><input type="button" value="Subscribe"/></li>
+        </ul>
+      </div>
+    </div>
+    <div class="bottom-details">
+      <div class="bottom_text">
+        <span class="copyright_text">Copyright © 2024 <a href="#">Krishi Mandi.</a>All rights reserved</span>
+        <span class="policy_terms">
+          <a href="#">Privacy policy</a>
+          <a href="#">Terms & condition</a>
+        </span>
+      </div>
+    </div>
+  </footer>
+
+</body>
+      
+    </div>
   )
 }
 
